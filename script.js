89128d54fce11c747718f6ea704d76baa0f94d66
satyam prajapati <satyamprajapati1979@gmail.com>
2024-11-21 11:13:20 +0530

    chnaging api and limit to fact 150 word and adding css animation

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const getFact =  async () => {
-const fact = await fetch('https://meowfacts.herokuapp.com/');
+// const fact = await fetch('https://meowfacts.herokuapp.com/');
+const fact = await fetch('https://catfact.ninja/fact?max_length=150');
 const data =  await fact.json();
 console.log(data)
 const h2 = document.querySelector('h2');
-h2.innerText = data.data[0]
+h2.innerText = data.fact
 }
 window.addEventListener('load', getFact);
 document.getElementById('next').addEventListener('click', getFact)
