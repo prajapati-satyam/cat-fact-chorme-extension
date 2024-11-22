7334aa8144e6574a25f9f6f20a2d3a88c293fdd9
satyam prajapati <satyamprajapati1979@gmail.com>
2024-11-22 18:35:30 +0530

    limit fact limit 200 word, adding heading h1 and giving it some css

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const getFact =  async () => {
 // const fact = await fetch('https://meowfacts.herokuapp.com/');
-const fact = await fetch('https://catfact.ninja/fact?max_length=150');
+const fact = await fetch('https://catfact.ninja/fact?max_length=200');
 const data =  await fact.json();
 console.log(data)
 const h2 = document.querySelector('h2');
